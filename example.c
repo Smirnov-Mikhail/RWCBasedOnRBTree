2c92fc99c0bff87d3f74713ec3fd8b9e215478c6
Smirnov-Mikhail <sm_m_a@mail.ru>
2015-10-23 14:27:59 +0300

    Read the file, then save the data to another file.

---
+++ example.c
@@ -0,0 +1,65 @@
+#include <linux/module.h> 
+#include <linux/fs.h> 
+#include <linux/sched.h> 
+#include <linux/uaccess.h> 
+
+// Path to input file.
+static char* file = NULL; 
+module_param(file, charp, 0); 
+
+// Path to output file.
+static char* log = NULL; 
+module_param(log, charp, 0); 
+
+#define BUFF_LEN 255 
+#define DEFNAME "inputFile.txt"; // Default value for input file.
+#define DEFLOG "./module.log" // Default value for output file.
+
+static int __init kread_init(void) { 
+	struct file *f; 
+	size_t n;
+	char buff[BUFF_LEN + 1] = DEFNAME; // Using the default value. 
+
+	if(file != NULL) // If set module parameter "file".
+		strcpy(buff, file);
+
+	f = filp_open(buff, O_RDONLY, 0); // Open the file for read.
+
+	if(IS_ERR(f)) { 
+		printk("file open failed: %s\n", buff); 
+		return -ENOENT; 
+	}
+
+	n = kernel_read(f, 0, buff, BUFF_LEN);
+
+	filp_close(f, NULL); // Close the file.
+	
+	loff_t offset = 0; 
+	mm_segment_t fs;
+	char buffOutput[BUFF_LEN + 1] = DEFLOG; // Using the default value.
+
+	if(log != NULL) // If set module parameter "log".
+		strcpy(buffOutput, log); 
+
+   	f = filp_open(buffOutput, O_CREAT | O_RDWR | O_TRUNC, S_IRUSR | S_IWUSR); // Open the file for write.
+
+	if(IS_ERR(f)) { 
+		printk("file open failed: %s\n", buff2); 
+		return -ENOENT; 
+	}
+ 
+	fs = get_fs(); 
+	set_fs(get_ds()); // Without it does not work.
+	strcpy(buffOutput, buff); // Without it does not work.
+	if((n = vfs_write(f, buffOutput, strlen(buffOutput), &offset)) != strlen(buffOutput)) { 
+		printk("failed to write: %d\n", n); 
+		return -EIO; 
+	} 
+	set_fs(fs); // Without it does not work.
+	
+	filp_close(f, NULL); // Close the file.
+
+	return -EPERM; 
+} 
+
+module_init(kread_init);
