afc619e31a8045152d6ca3c0ecd16f42bf5fb2c2
Smirnov-Mikhail <sm_m_a@mail.ru>
2015-10-26 17:45:26 +0300

    Change reading from file.

--- example.c
+++ example.c
@@ -4,6 +4,9 @@
 #include <linux/uaccess.h> 
 #include <linux/string.h> 
 
+// Number of bits for reading.
+int numberOfBits = 10;
+
 // Path to input file.
 static char* file = NULL; 
 module_param(file, charp, 0); 
@@ -20,31 +23,26 @@ char pathToOutputFile[BUFF_LEN + 1] = DEFLOG;
 
 struct file *fileInput;
 struct file *fileOutput; 
-size_t n;
 
-char buff[BUFF_LEN + 1]; // Using the default value.
-char str[BUFF_LEN + 1];
-char buffOutput[BUFF_LEN + 1]; // Using the default value.
-char strNumber[11] = "New line: ";
-
-loff_t offset = 0; // For write.
-loff_t file_offset = 0; // For read.
-mm_segment_t fs;
-size_t n; 
-long move;
-long length;
-
-int i = 0;
-int cur = 0;
-int numberOfStr = 0;
+char buff[BUFF_LEN + 1]; // Buffer for reading the memory unit from file.
+char str[BUFF_LEN + 1]; // Buffer for forming one line.
 
 // Check that the file is opened incorrectly.
-bool isOpenIncorrect(struct file *file, char pathToFile[]);
+bool isOpenIncorrect(struct file *file, char pathToFile[], mm_segment_t fs);
 
 // Check that the file is empty.
-bool fileIsEmpty(struct file *file, char pathToFile[]);
+bool fileIsEmpty(struct file *file, char pathToFile[], long *length);
 
-static int __init kread_init(void) { 
+static int __init kread_init(void) {
+	int j = 0;
+	int i = 0;
+	size_t n;
+
+	loff_t offset = 0; // For write.
+	loff_t file_offset = 0; // For read.
+	mm_segment_t fs;
+	long move; // Step at reading.
+
 	fs = get_fs(); 
 	set_fs(get_ds()); 
 
@@ -53,39 +51,46 @@ static int __init kread_init(void) {
 
 	fileInput = filp_open(pathToInputFile, O_RDONLY, 0); // Open the file for read.
 	
-	if (isOpenIncorrect(fileInput, pathToInputFile))
+	if (isOpenIncorrect(fileInput, pathToInputFile, fs))
 		return -ENOENT;
-
-	if (fileIsEmpty(fileInput, pathToInputFile))
-		return (int)length; 
-
-	printk("file size = %d bytes\n", (int)length);
 
 	if (log != NULL) // If set module parameter "log".
 		strcpy(pathToOutputFile, log); 
 
    	fileOutput = filp_open(pathToOutputFile, O_CREAT | O_RDWR | O_TRUNC, S_IRUSR | S_IWUSR); // Open the file for write.
 	
-	if (isOpenIncorrect(fileOutput, pathToOutputFile))
+	if (isOpenIncorrect(fileOutput, pathToOutputFile, fs))
 		return -ENOENT;
-
-	move = vfs_llseek(fileInput, 1L, 0); // 0 means SEEK_SET. (set in begin of file)
-
-	for (cur = 0; cur < length; cur++) {
-		vfs_read(fileInput, buff, move, &file_offset);
-		if (strcmp(buff, "\n") == 0) {
-			vfs_write(fileOutput, strNumber, strlen(strNumber), &offset);
-			vfs_write(fileOutput, str, strlen(str), &offset); // Main string.
-			vfs_write(fileOutput, "\n", 1, &offset);
-			i = 0;
-			strcpy(str, ""); // Clean the string.
-			numberOfStr++;
+	
+	move = vfs_llseek(fileInput, numberOfBits, 0); // 0 means SEEK_SET. (set in begin of file
+	while (1) {
+		n = vfs_read(fileInput, buff, move, &file_offset);
+		if (n == 0) { // If the file is ended.
+			if (strlen(str) > 0) {
+				printk("ALO %s\n", str);
+				vfs_write(fileOutput, "New line: ", 10, &offset);
+				vfs_write(fileOutput, str, strlen(str), &offset);
+			}
+			break;
 		}
-		else {
-			printk("%s", buff);	
-			i++;
-			strcat(str, buff);
+
+		for (j = 0; j < n; j++) {
+			if (buff[j] == '\n') {
+				printk("buff %s\n", buff);
+				printk("lol %s\n", str);
+				vfs_write(fileOutput, "New line: ", 10, &offset);
+				vfs_write(fileOutput, str, strlen(str), &offset); // Main string.
+				vfs_write(fileOutput, "\n", 1, &offset);
+				i = 0;
+				strcpy(str, ""); // Clean the string.
+			}
+			else {		
+				str[i] = buff[j];
+				str[i + 1] = '\0';
+				i++;
+			}
 		}
+		printk("APY %s\n", str);
 	}
 	set_fs(fs);
 
@@ -95,7 +100,7 @@ static int __init kread_init(void) {
 	return -EPERM; 
 }
 
-bool isOpenIncorrect(struct file *file, char pathToFile[]) {
+bool isOpenIncorrect(struct file *file, char pathToFile[], mm_segment_t fs) {
 	if (IS_ERR(file)) { // Check that the input file is opened correctly.
 		printk("file open failed: %s\n", pathToFile); 
 		set_fs(fs);
@@ -105,11 +110,11 @@ bool isOpenIncorrect(struct file *file, char pathToFile[]) {
 	return false;
 }
 
-bool fileIsEmpty(struct file *file, char pathToFile[]) {
-	length = vfs_llseek(fileInput, 0L, 2); // 2 means SEEK_END. (set in end of file)
-	if (length <= 0) { // Check that the input file is not empty.
+bool fileIsEmpty(struct file *file, char pathToFile[], long *length) {
+	*length = vfs_llseek(fileInput, 0L, 2); // 2 means SEEK_END. (set in end of file)
+	if (*length <= 0) { // Check that the input file is not empty.
 		printk( "failed to lseek %s\n", pathToInputFile); 
-		length = -EINVAL; 
+		*length = -EINVAL; 
 		return true; 
 	}
 
